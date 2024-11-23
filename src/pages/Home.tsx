2c1ab62b38237db04017c8f96406d77a328af163
Immaculate Nyoni <sthwaloe@gmail.com>
2024-11-24 01:55:36 +0200

    build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import { motion } from 'framer-motion';
 import { ArrowRight, Code, Calculator, Truck, Cpu } from 'lucide-react';
 import { Link } from 'react-router-dom';
